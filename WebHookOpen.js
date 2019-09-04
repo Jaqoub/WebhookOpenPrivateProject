a29b2015005596f8c9c3b91157b9ee803ca71f1d
Jaqoub <33924194+Jaqoub@users.noreply.github.com>
2019-09-04 09:40:37 +0200

    Initial commit

---
+++ WebHookOpen.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+var sender = null;
+var committer = null;
+
+app.post('/', function(request){
+    const repository = request.body["repository"];
+    owner = request.body["push"];
+    committer = request.body["commit"];
+
+    if(OpenOrClosed() == false){
+
+    }
+}
